9a60562ed41e58dd4a1df4b229f956b0e3d3009f
GunyahJohn1 <noahjohn1407@outlook.com>
2025-03-31 10:29:37 +1100

    Update LuaUI.hpp

--- app/src/main/cpp/LuaUI.hpp
+++ app/src/main/cpp/LuaUI.hpp
@@ -4,131 +4,109 @@
 
 // Lua UI we execute
 std::string LuaUI = R"(
--- Gui to Lua
--- Version: 3.2
 
--- Instances:
-
-local MobileBlox = Instance.new('ScreenGui')
-local Main = Instance.new('Frame')
-local TextBox = Instance.new('TextBox')
-local Clear = Instance.new('TextButton')
-local Execute = Instance.new('TextButton')
+local MobileBlox = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local btn = Instance.new("TextButton")
+local box = Instance.new("TextBox")
+local btn_2 = Instance.new("TextButton")
+local ImageLabel = Instance.new("ImageLabel")
 
 --Properties:
 
 MobileBlox.Name = "MobileBlox"
-MobileBlox.Parent = game:WaitForChild('CoreGui')
+MobileBlox.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+MobileBlox.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
-Main.Name = "Main"
-Main.Parent = MobileBlox
-Main.BackgroundColor3 = Color3.fromRGB(50, 50, 50)
-Main.Position = UDim2.new(0.318584085, 0, 0.197959185, 0)
-Main.Size = UDim2.new(0, 492, 0, 282)
+Frame.Parent = MobileBlox
+Frame.BackgroundColor3 = Color3.fromRGB(70, 71, 71)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.218377084, 0, 0.254838705, 0)
+Frame.Size = UDim2.new(0, 964, 0, 608)
+Frame.Style = Enum.FrameStyle.RobloxRound
 
-TextBox.Parent = Main
-TextBox.BackgroundColor3 = Color3.fromRGB(33, 33, 33)
-TextBox.Position = UDim2.new(0.0375582054, 0, 0.0874655992, 0)
-TextBox.Size = UDim2.new(0, 450, 0, 195)
-TextBox.ClearTextOnFocus = false
-TextBox.Font = Enum.Font.Ubuntu
-TextBox.MultiLine = true
-TextBox.Text = ""
-TextBox.TextColor3 = Color3.fromRGB(186, 186, 186)
-TextBox.TextSize = 14.000
-TextBox.TextXAlignment = Enum.TextXAlignment.Left
-TextBox.TextYAlignment = Enum.TextYAlignment.Top
+btn.Name = "btn"
+btn.Parent = Frame
+btn.BackgroundColor3 = Color3.fromRGB(155, 157, 157)
+btn.BorderColor3 = Color3.fromRGB(0, 0, 0)
+btn.BorderSizePixel = 0
+btn.Position = UDim2.new(0.0305508822, 0, 0.887107432, 0)
+btn.Size = UDim2.new(0, 200, 0, 50)
+btn.Style = Enum.ButtonStyle.RobloxRoundButton
+btn.Font = Enum.Font.SourceSans
+btn.Text = "Execute"
+btn.TextColor3 = Color3.fromRGB(0, 0, 0)
+btn.TextSize = 14.000
 
-Clear.Name = "Clear"
-Clear.Parent = Main
-Clear.BackgroundColor3 = Color3.fromRGB(144, 0, 0)
-Clear.Position = UDim2.new(0.544715464, 0, 0.822695017, 0)
-Clear.Size = UDim2.new(0, 200, 0, 50)
-Clear.Font = Enum.Font.SourceSans
-Clear.Text = "Clear"
-Clear.TextColor3 = Color3.fromRGB(0, 0, 0)
-Clear.TextScaled = true
-Clear.TextSize = 14.000
-Clear.TextWrapped = true
+box.Name = "box"
+box.Parent = Frame
+box.BackgroundColor3 = Color3.fromRGB(164, 166, 166)
+box.BackgroundTransparency = 0.500
+box.BorderColor3 = Color3.fromRGB(0, 0, 0)
+box.BorderSizePixel = 0
+box.Position = UDim2.new(0.00936826319, 0, 0.0244906582, 0)
+box.Size = UDim2.new(0, 924, 0, 489)
+box.Font = Enum.Font.Unknown
+box.Text = ""
+box.TextColor3 = Color3.fromRGB(0, 0, 0)
+box.TextSize = 14.000
+box.TextXAlignment = Enum.TextXAlignment.Left
+box.TextYAlignment = Enum.TextYAlignment.Top
 
-Execute.Name = "Execute"
-Execute.Parent = Main
-Execute.BackgroundColor3 = Color3.fromRGB(63, 190, 93)
-Execute.Position = UDim2.new(0.0365853645, 0, 0.822695017, 0)
-Execute.Size = UDim2.new(0, 200, 0, 50)
-Execute.Font = Enum.Font.SourceSans
-Execute.Text = "Execute"
-Execute.TextColor3 = Color3.fromRGB(0, 0, 0)
-Execute.TextScaled = true
-Execute.TextSize = 14.000
-Execute.TextWrapped = true
+btn_2.Name = "btn"
+btn_2.Parent = Frame
+btn_2.BackgroundColor3 = Color3.fromRGB(155, 157, 157)
+btn_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+btn_2.BorderSizePixel = 0
+btn_2.Position = UDim2.new(0.742626011, 0, 0.876204669, 0)
+btn_2.Size = UDim2.new(0, 200, 0, 50)
+btn_2.Style = Enum.ButtonStyle.RobloxRoundButton
+btn_2.Font = Enum.Font.SourceSans
+btn_2.Text = "Clear"
+btn_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+btn_2.TextSize = 14.000
+
+ImageLabel.Parent = MobileBlox
+ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+ImageLabel.BackgroundTransparency = 1.000
+ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageLabel.BorderSizePixel = 0
+ImageLabel.Position = UDim2.new(0.470167071, 0, 0.730912566, 0)
+ImageLabel.Size = UDim2.new(0, 100, 0, 96)
+ImageLabel.Image = "http://www.roblox.com/asset/?id=137134606494396"
 
 -- Scripts:
 
-local function BGWS_fake_script() -- Main.DragScript
-	local script = Instance.new('LocalScript', Main)
+local function XHYNWKD_fake_script() -- btn.Script 
+	local script = Instance.new('Script', btn)
 
-	--Not made by me, check out this video: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
-	--Put this inside of your Frame and configure the speed if you would like.
-	--Enjoy! Credits go to: https://www.youtube.com/watch?v=z25nyNBG7Js&t=22s
-
-	local UIS = game:GetService('UserInputService')
-	local frame = script.Parent
-	local dragToggle = nil
-	local dragSpeed = 0.25
-	local dragStart = nil
-	local startPos = nil
-
-	local function updateInput(input)
-		local delta = input.Position - dragStart
-		local position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X,
-			startPos.Y.Scale, startPos.Y.Offset + delta.Y)
-		game:GetService('TweenService'):Create(frame, TweenInfo.new(dragSpeed), {Position = position}):Play()
+	coroutine.wrap(NBUT_fake_script)()
+	local function OBDPHLQ_fake_script() -- Execute.LocalScript
+		local script = Instance.new('LocalScript', Execute)
+	
+		local box = script.Parent.Parent.TextBox
+		local btn = script.Parent
+	
+		btn.MouseButton1Click:Connect(function()
+			loadstring(box.Text)()
+		end)
 	end
-
-	frame.InputBegan:Connect(function(input)
-		if (input.UserInputType == Enum.UserInputType.MouseButton1 or input.UserInputType == Enum.UserInputType.Touch) then
-			dragToggle = true
-			dragStart = input.Position
-			startPos = frame.Position
-			input.Changed:Connect(function()
-				if input.UserInputState == Enum.UserInputState.End then
-					dragToggle = false
-				end
-			end)
-		end
-	end)
-
-	UIS.InputChanged:Connect(function(input)
-		if input.UserInputType == Enum.UserInputType.MouseMovement or input.UserInputType == Enum.UserInputType.Touch then
-			if dragToggle then
-				updateInput(input)
-			end
-		end
-	end)
-
 end
-coroutine.wrap(BGWS_fake_script)()
-local function NBUT_fake_script() -- Clear.LocalScript
-	local script = Instance.new('LocalScript', Clear)
+coroutine.wrap(XHYNWKD_fake_script)()
+local function WAAD_fake_script() -- btn_2.Script 
+	local script = Instance.new('Script', btn_2)
 
-	local box = script.Parent.Parent.TextBox
-	local btn = script.Parent
-
-	btn.MouseButton1Click:Connect(function()
-		box.Text = "";
-	end)
+	coroutine.wrap(BGWS_fake_script)()
+	local function NBUT_fake_script() -- Clear.LocalScript
+		local script = Instance.new('LocalScript', Clear)
+	
+		local box = script.Parent.Parent.TextBox
+		local btn = script.Parent
+	
+		btn.MouseButton1Click:Connect(function()
+			box.Text = "";
+		end)
+	end
 end
-coroutine.wrap(NBUT_fake_script)()
-local function OBDPHLQ_fake_script() -- Execute.LocalScript
-	local script = Instance.new('LocalScript', Execute)
-
-	local box = script.Parent.Parent.TextBox
-	local btn = script.Parent
-
-	btn.MouseButton1Click:Connect(function()
-		loadstring(box.Text)()
-	end)
-end
-coroutine.wrap(OBDPHLQ_fake_script)()
-)";
+coroutine.wrap(WAAD_fake_script)()
